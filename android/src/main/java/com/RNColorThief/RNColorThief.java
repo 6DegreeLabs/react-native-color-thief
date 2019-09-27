1cff5f5d9c98fb3a2a640988d78c08f340ff385f
Clinton Robinson <clinton.robinson@gmail.com>
2019-09-27 10:52:08 -0400

    Adding crop to image

--- android/src/main/java/com/RNColorThief/RNColorThief.java
+++ android/src/main/java/com/RNColorThief/RNColorThief.java
@@ -12,88 +12,214 @@
  * ------
  * Lokesh Dhakar - for the original Color Thief JavaScript version
  * available at http://lokeshdhakar.com/projects/color-thief/
- * 
+ *
  * Sven Woltmann - for the fast Java Implementation of Color Thief
- * available at https://github.com/SvenWoltmann/color-thief-java 
+ * available at https://github.com/SvenWoltmann/color-thief-java
  */
 
 package com.RNColorThief;
 
-import java.net.URL;
-import java.io.IOException;
-
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Base64;
+import java.util.Arrays;
 
-public class RNColorThief {
+import com.RNColorThief.MMCQ.CMap;
 
-    public static int[] getColor(String imageUrl) {
-        Bitmap sourceImage = RNColorThief.retrieveImageFromUrl(imageUrl);
-        if (sourceImage == null) {
+public class ColorThief {
+
+    private static final int DEFAULT_QUALITY = 10;
+    private static final boolean DEFAULT_IGNORE_WHITE = true;
+
+    /**
+     * Use the median cut algorithm to cluster similar colors and return the base color from the
+     * largest cluster.
+     *
+     * @param sourceImage
+     *            the source image
+     *
+     * @return the dominant color as RGB array
+     */
+    public static int[] getColor(Bitmap sourceImage) {
+        int[][] palette = getPalette(sourceImage, 5);
+        if (palette == null) {
             return null;
         }
-
-		return ColorThief.getColor(sourceImage);
-    }
-
-    public static int[] getColor(String imageUrl, int quality, boolean ignoreWhite) {
-        Bitmap sourceImage = RNColorThief.retrieveImageFromUrl(imageUrl);
-        if (sourceImage == null) {
-            return null;
-        }
-
-		return ColorThief.getColor(sourceImage, quality, ignoreWhite);
-    }
-
-    public static int[][] getPalette(String imageUrl, int colorCount) {
-        Bitmap sourceImage = RNColorThief.retrieveImageFromUrl(imageUrl);
-        if (sourceImage == null) {
-            return null;
-        }
-
-		return ColorThief.getPalette(sourceImage, colorCount);
-    }
-
-    public static int[][] getPalette(
-			String imageUrl,
-            int colorCount,
-            int quality,
-            boolean ignoreWhite) {
-
-		Bitmap sourceImage = RNColorThief.retrieveImageFromUrl(imageUrl);
-        if (sourceImage == null) {
-            return null;
-        }
-
-		return ColorThief.getPalette(sourceImage, colorCount, quality, ignoreWhite);
+        int[] dominantColor = palette[0];
+        return dominantColor;
     }
 
     /**
-     * Retrieve image source from remote url.
+     * Use the median cut algorithm to cluster similar colors and return the base color from the
+     * largest cluster.
      *
-     * @param imageUrl
-     *          the image url
+     * @param sourceImage
+     *            the source image
+     * @param quality
+     *            1 is the highest quality settings. 10 is the default. There is a trade-off between
+     *            quality and speed. The bigger the number, the faster a color will be returned but
+     *            the greater the likelihood that it will not be the visually most dominant color.
+     * @param ignoreWhite
+     *            if <code>true</code>, white pixels are ignored
      *
-     * @return the image as a Bitmap
+     * @return the dominant color as RGB array
+     * @throws IllegalArgumentException
+     *             if quality is &lt; 1
      */
-    private static Bitmap retrieveImageFromUrl(String imageUrl) {
-        if (imageUrl.startsWith("data:image")) {
-            String base64Image = imageUrl.split(",")[1];
-
-            byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
-            Bitmap image = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-            return image;
+    public static int[] getColor(Bitmap sourceImage, int quality, boolean ignoreWhite) {
+        int[][] palette = getPalette(sourceImage, 5, quality, ignoreWhite);
+        if (palette == null) {
+            return null;
         }
-        else {
-            try {
-                URL url = new URL(imageUrl);
-                Bitmap image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-                return image;
-            } catch (IOException e) {
-                System.out.println(e);
-                return null;
+        int[] dominantColor = palette[0];
+        return dominantColor;
+    }
+
+    /**
+     * Use the median cut algorithm to cluster similar colors.
+     *
+     * @param sourceImage
+     *            the source image
+     * @param colorCount
+     *            the size of the palette; the number of colors returned
+     *
+     * @return the palette as array of RGB arrays
+     */
+    public static int[][] getPalette(Bitmap sourceImage, int colorCount) {
+        CMap cmap = getColorMap(sourceImage, colorCount);
+        if (cmap == null) {
+            return null;
+        }
+        return cmap.palette();
+    }
+
+    /**
+     * Use the median cut algorithm to cluster similar colors.
+     *
+     * @param sourceImage
+     *            the source image
+     * @param colorCount
+     *            the size of the palette; the number of colors returned
+     * @param quality
+     *            1 is the highest quality settings. 10 is the default. There is a trade-off between
+     *            quality and speed. The bigger the number, the faster the palette generation but
+     *            the greater the likelihood that colors will be missed.
+     * @param ignoreWhite
+     *            if <code>true</code>, white pixels are ignored
+     *
+     * @return the palette as array of RGB arrays
+     * @throws IllegalArgumentException
+     *             if quality is &lt; 1
+     */
+    public static int[][] getPalette(
+            Bitmap sourceImage,
+            int colorCount,
+            int quality,
+            boolean ignoreWhite) {
+        CMap cmap = getColorMap(sourceImage, colorCount, quality, ignoreWhite);
+        if (cmap == null) {
+            return null;
+        }
+        return cmap.palette();
+    }
+
+    /**
+     * Use the median cut algorithm to cluster similar colors.
+     *
+     * @param sourceImage
+     *            the source image
+     * @param colorCount
+     *            the size of the palette; the number of colors returned (minimum 2, maximum 256)
+     *
+     * @return the color map
+     */
+    public static CMap getColorMap(Bitmap sourceImage, int colorCount) {
+        return getColorMap(sourceImage, colorCount, DEFAULT_QUALITY, DEFAULT_IGNORE_WHITE);
+    }
+
+    /**
+     * Use the median cut algorithm to cluster similar colors.
+     *
+     * @param sourceImage
+     *            the source image
+     * @param colorCount
+     *            the size of the palette; the number of colors returned (minimum 2, maximum 256)
+     * @param quality
+     *            1 is the highest quality settings. 10 is the default. There is a trade-off between
+     *            quality and speed. The bigger the number, the faster the palette generation but
+     *            the greater the likelihood that colors will be missed.
+     * @param ignoreWhite
+     *            if <code>true</code>, white pixels are ignored
+     *
+     * @return the color map
+     * @throws IllegalArgumentException
+     *             if quality is &lt; 1
+     */
+    public static CMap getColorMap(
+            Bitmap sourceImage,
+            int colorCount,
+            int quality,
+            boolean ignoreWhite) {
+        if (colorCount < 2 || colorCount > 256) {
+            throw new IllegalArgumentException("Specified colorCount must be between 2 and 256.");
+        }
+        if (quality < 1) {
+            throw new IllegalArgumentException("Specified quality should be greater then 0.");
+        }
+
+        int[][] pixelArray = getPixels(sourceImage, quality, ignoreWhite);
+
+        // Send array to quantize function which clusters values using median cut algorithm
+        CMap cmap = MMCQ.quantize(pixelArray, colorCount);
+        return cmap;
+    }
+
+    /**
+     * Gets the image's pixels via Bitmap.getPixels(..).
+     *
+     * @param sourceImage
+     *            the source image
+     * @param quality
+     *            1 is the highest quality settings. 10 is the default. There is a trade-off between
+     *            quality and speed. The bigger the number, the faster the palette generation but
+     *            the greater the likelihood that colors will be missed.
+     * @param ignoreWhite
+     *            if <code>true</code>, white pixels are ignored
+     *
+     * @return an array of pixels (each an RGB int array)
+     */
+    private static int[][] getPixels(
+            Bitmap sourceImage,
+            int quality,
+            boolean ignoreWhite) {
+        int width = sourceImage.getWidth();
+        int height = sourceImage.getHeight();
+
+        int pixelCount = width * height;
+
+        // numRegardedPixels must be rounded up to avoid an ArrayIndexOutOfBoundsException if all
+        // pixels are good.
+        int numRegardedPixels = (pixelCount + quality - 1) / quality;
+
+        int numUsedPixels = 0;
+
+        int[][] res = new int[numRegardedPixels][];
+        int r, g, b;
+
+        int[] pixels = new int[pixelCount];
+        sourceImage.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        for (int i = 0; i < pixelCount; i += quality) {
+            int argb = pixels[i];
+
+            r = (argb >> 16) & 0xFF;
+            g = (argb >> 8) & 0xFF;
+            b = (argb) & 0xFF;
+            if (!(ignoreWhite && r > 250 && g > 250 && b > 250)) {
+                res[numUsedPixels] = new int[] {r, g, b};
+                numUsedPixels++;
             }
         }
+
+        return Arrays.copyOfRange(res, 0, numUsedPixels);
     }
+
 }
