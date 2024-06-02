a2a868eb7b5a4607d1949fc0c91cf51e630a4de7
Levon <levon.avetisyan1998@gmail.com>
2024-06-03 00:22:37 +0200

    added web

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+import { NativeModules, Platform } from 'react-native';
 
-import { NativeModules } from 'react-native';
+let RNColorThief;
 
-const { RNColorThief } = NativeModules;
+if (Platform.OS === 'web') {
+    const ColorThief = require('@pioug/colorthief');
+
+    RNColorThief = {
+        getColor: async (imgElement, quality = 10, ignoreWhite = false) => {
+            const colorThief = new ColorThief();
+            const [r, g, b] = await colorThief.getColor(imgElement, quality, ignoreWhite);
+            return { r, g, b };
+        },
+        getPalette: async (imgElement, colorCount = 10, quality = 10, ignoreWhite = false) => {
+            const colorThief = new ColorThief();
+            const palette = await colorThief.getPalette(imgElement, colorCount, quality, ignoreWhite);
+            return palette.map(([r, g, b]) => ({ r, g, b }));
+        },
+    };
+} else {
+    const { RNColorThief: NativeColorThief } = NativeModules;
+
+    RNColorThief = {
+        getColor: async (source, quality = 10, ignoreWhite = false) => {
+            const color = await NativeColorThief.getColor(source, quality, ignoreWhite);
+            return color;
+        },
+        getPalette: async (source, colorCount = 10, quality = 10, ignoreWhite = false) => {
+            const palette = await NativeColorThief.getPalette(source, colorCount, quality, ignoreWhite);
+            return palette;
+        },
+    };
+}
 
 export default RNColorThief;
